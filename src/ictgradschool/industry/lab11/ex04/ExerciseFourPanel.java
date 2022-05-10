7754034949ce374d3d1395c0c4cfcc3f02dc3a4e
Taneya <twil609@aucklanduni.ac.nz>
2022-05-10 20:30:12 +1000

    exercise 4

--- src/ictgradschool/industry/lab11/ex04/ExerciseFourPanel.java
+++ src/ictgradschool/industry/lab11/ex04/ExerciseFourPanel.java
@@ -4,14 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Displays an animated balloon.
  */
-public class ExerciseFourPanel extends JPanel implements ActionListener {
+public class ExerciseFourPanel extends JPanel implements ActionListener, KeyListener {
 
     private  Balloon balloon;
-    private  JButton moveButton;
+//    private  JButton moveButton;
+    private Timer timer;
 
     /**
      * Creates a new ExerciseFourPanel.
@@ -21,11 +24,43 @@ public class ExerciseFourPanel extends JPanel implements ActionListener {
 
         this.balloon = new Balloon(30, 60);
 
-        this.moveButton = new JButton("Move balloon");
-        this.moveButton.addActionListener(this);
-        this.add(moveButton);
+        addKeyListener(this);
+
+        timer = new Timer(10, this);
+
+//        this.moveButton = new JButton("Move balloon");
+//        this.moveButton.addActionListener(this);
+//        this.add(moveButton);
 
     }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (!timer.isRunning()) {
+            timer.start();
+        }
+
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+                balloon.setDirection(Direction.Left);
+                break;
+            case KeyEvent.VK_RIGHT:
+                balloon.setDirection(Direction.Right);
+                break;
+            case KeyEvent.VK_UP:
+                balloon.setDirection(Direction.Up);
+                break;
+            case KeyEvent.VK_DOWN:
+                balloon.setDirection(Direction.Down);
+                break;
+            case KeyEvent.VK_S:
+                timer.stop();
+        }
+    }
+
+
 
     /**
      * Moves the balloon and calls repaint() to tell Swing we need to redraw ourselves.
@@ -35,6 +70,7 @@ public class ExerciseFourPanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         balloon.move();
+
 
         // Sets focus to the panel itself, rather than the JButton. This way, the panel can continue to generate key
         // events even after we've clicked the button.
@@ -55,5 +91,17 @@ public class ExerciseFourPanel extends JPanel implements ActionListener {
         
         // Sets focus outside of actionPerformed so key presses work without pressing the button
         requestFocusInWindow();
+    }
+
+
+    // unused implemented methods
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
